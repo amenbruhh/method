71d225d4cc5b11232c423517fb996d3c2c9ea3e3
amenbruhh <pachison.artyom@yandex.ru>
2024-07-09 01:22:39 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,62 @@
+import java.time.LocalDate;
+
 public class Main {
+
+    // задача 1
+    public static void checkYear(int year) {
+        if (year % 4 == 0) {
+            System.out.println(year + " год - является високосным.");
+        } else {
+            System.out.println(year + " год - не является високосным.");
+        }
+    }
+
+    // задача 2
+    public static void printAppVersion(int clientOS) {
+
+        int currentYear = LocalDate.now().getYear();
+
+        if (clientOS == 0 && currentYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS != 0) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (currentYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    // задача 3
+    public static void calculateDeliveryDays(int deliveryDistance) {
+
+        int deliveryDays;
+
+        if (deliveryDistance <= 20) {
+            deliveryDays = 1;
+        } else if (deliveryDistance <= 60) {
+            deliveryDays = 2;
+        } else if (deliveryDistance <= 100) {
+            deliveryDays = 3;
+        } else {
+            System.out.println("К сожалению, доставка недоступна");
+            return;
+        }
+        System.out.println("Потребуется дней: " + deliveryDays);
+
+    }
+
     public static void main(String[] args) {
 
+        int year = 2023;
+        checkYear(year);
+        System.out.println();
+
+        int clientOS = 0;
+        printAppVersion(clientOS);
+        System.out.println();
+
+        int deliveryDistance = 1000;
+        calculateDeliveryDays(deliveryDistance);
     }
 }
